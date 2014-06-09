572ad968de3df5c7b77c3a546ca3b42bd92abf29
PhuongNM4 <live_red_river@yahoo.com>
2014-06-09 16:35:02 +0700

    Update controllers.js

--- js/controllers.js
+++ js/controllers.js
@@ -9,49 +9,61 @@ angular.module('codeblockControllers', [])
         $location.hash('top');
         $anchorScroll();
     };
-    
-     $scope.resetFilter = function () {
+
+    $scope.resetFilter = function () {
         $scope.query = '';
         $location.hash('top');
         $anchorScroll();
     }
 })
 
-//CreateController
-.controller('CreateCTRL', function ($scope, $location, $timeout, CodeblocksFactory, rootUrl) {
-    $scope.addMode = true;
-    $scope.db = CodeblocksFactory.getData(rootUrl);
+    //CreateController
+    .controller('CreateCTRL', function ($scope, $location, $timeout, CodeblocksFactory, rootUrl) {
+        $scope.addMode = true;
+        $scope.db = CodeblocksFactory.getData(rootUrl);
 
-    $scope.saveBlock = function () {
-        $scope.block.id = $scope.db.$getIndex().length + 1;
-        $scope.block.time = GetDateTimeNow();
+        $scope.saveBlock = function () {
+            $scope.block.id = $scope.db.$getIndex().length + 1;
+            $scope.block.time = GetDateTimeNow();
 
-        $scope.db.$add($scope.block, function () {
-            $timeout(function () {
-                $location.path('/');
+            $scope.db.$add($scope.block, function () {
+                $timeout(function () {
+                    $location.path('/');
+                });
             });
-        });
-    };
-})
+        };
+    })
 
-//EditController
-.controller('ViewCTRL', function ($scope, $location, $routeParams, CodeblocksFactory, rootUrl) {
-    $scope.editMode = false;
-    rootUrl = rootUrl + $routeParams.blockId;
-    $scope.block = CodeblocksFactory.getData(rootUrl);
+    //EditController
+    .controller('ViewCTRL', function ($scope, $location, $routeParams, CodeblocksFactory, CommentFactory, rootUrl) {
+        rootUrl = rootUrl + $routeParams.blockId;
 
-    $scope.toggleEdit = function () {
-        $scope.editMode = !$scope.editMode;
-    };
+        $scope.editMode = false;
+        $scope.showComment = false;
+        $scope.block = CodeblocksFactory.getData(rootUrl);
+        $scope.comments = CommentFactory.getComments(rootUrl);
 
-    $scope.destroy = function () {
-        $scope.block.$remove();
-        $location.path('/');
-    };
+        $scope.toggleEdit = function () {
+            $scope.editMode = !$scope.editMode;
+        };
 
-    $scope.saveBlock = function () {
-        $scope.block.$save();
-        $location.path('/');
-    };
-});
+        $scope.toggleComment = function () {
+            $scope.showComment = !$scope.showComment;
+        }
+
+        $scope.destroy = function () {
+            $scope.block.$remove();
+            $location.path('/');
+        };
+
+        $scope.saveBlock = function () {
+            $scope.block.$save();
+            $location.path('/');
+        };
+
+        $scope.addComment = function () {
+            $scope.comments.$add($scope.newCmt);
+            $scope.newCmt.content = "";
+        }
+    });
 
