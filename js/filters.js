5290e8d95dfbb7d5d129d64991d318918746a513
PhuongNM4 <live_red_river@yahoo.com>
2014-06-09 09:01:57 +0700

    Update filters.js

--- js/filters.js
+++ js/filters.js
@@ -4,7 +4,7 @@ angular.module('codeblockFilters', []).filter('splitTags', function () {
         var arrStr = input.split(',');
         var returnStr = "";
         for (var i = 0; i < arrStr.length; i++) {
-            returnStr = returnStr + "<code ng-click='filterByTag('"+arrStr[i]+"')'>" + arrStr[i] + "</code>";
+            returnStr = returnStr + "<code ng-click='filterByTag("+arrStr[i]+")'>" + arrStr[i] + "</code>";
         }
         return returnStr;
     };
